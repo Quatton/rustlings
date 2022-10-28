499476cbe363908562140b97ed5eb8aa9974d7c5
Quatton <atomicatives@gmail.com>
2022-10-28 23:48:14 +0900

    standard_library_types

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,8 +1,6 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
     // Do not use:
@@ -10,9 +8,14 @@ pub fn factorial(num: u64) -> u64 {
     // Try not to use:
     // - imperative style loops (for, while)
     // - additional variables
-    // For an extra challenge, don't use:
+    // For an extra c hallenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    if num == 0 {
+        1
+    } else {
+        (1..=num).fold(1, |acc, x| acc * x)
+    }
 }
 
 #[cfg(test)]
